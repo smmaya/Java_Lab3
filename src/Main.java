362c84241994e09fa634cf0e222f449c20a045dc
Sławomir Majchrzak <smmaya@yahoo.fr>
2021-12-05 11:09:04 +0100

    zad 2

--- src/Main.java
+++ src/Main.java
@@ -5,15 +5,17 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        Student[] students = new Student[3];
+        Student studentA = new Student("Adam", "Słodowy", 123443, true);
+        students[0] = studentA;
+        studentA = new Student("Bartek", "Iksiński", 987304, false);
+        students[1] = studentA;
+        studentA = new Student("Daria", "Iglasta", 998701, true);
+        students[2] = studentA;
 
-
-        /* ćw 2 klasy */
-        /* zad.1
-            a) Stworzyć publiczną klasę Student z 2-ma polami String, 1 polem int i 1 polem boolean,
-            klasa ma się znajdować w osobnym pliku Student.java
-            b) stworzyć 3 objekty klasy student i wstawić je do tablicy
-            c) wyświetlić dane z tablicy, użyć pętli for lub forEach
-            d) dane powinny być estetycznie przedstawione
-         */
+        for (Student student : students) {
+            student.niceDisplay();
+            System.out.println("");
+        }
     }
 }
---
+++ src/Student.java
@@ -0,0 +1,24 @@
+
+public class Student {
+    public String firstName;
+    public String lastName;
+    public int numerKatalogu;
+    public boolean status;
+
+    public Student(String firstName, String lastName, int numerKatalogu, boolean status) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.numerKatalogu = numerKatalogu;
+        this.status = status;
+    }
+    public void niceDisplay(){
+        System.out.println("Imię : "+ this.firstName);
+        System.out.println("Nazwisko: "+ this.lastName);
+        System.out.println("Numer: "+ this.numerKatalogu);
+        if(!status){
+            System.out.println("Status: Nieaktywny");
+        } else {
+            System.out.println("Status: Aktywny");
+        }
+    }
+}
