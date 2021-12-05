d112c83b2f293a05a821159c148aad87cabce64d
Sławomir Majchrzak <smmaya@yahoo.fr>
2021-12-05 11:11:20 +0100

    zad 2

--- src/Main.java
+++ src/Main.java
@@ -6,12 +6,12 @@ public class Main {
 
     public static void main(String[] args) {
         Student[] students = new Student[3];
-        Student studentA = new Student("Adam", "Słodowy", 123443, true);
-        students[0] = studentA;
-        studentA = new Student("Bartek", "Iksiński", 987304, false);
-        students[1] = studentA;
-        studentA = new Student("Daria", "Iglasta", 998701, true);
-        students[2] = studentA;
+        Student stu = new Student("Adam", "Słodowy", 123443, true);
+        students[0] = stu;
+        stu = new Student("Bartek", "Iksiński", 987304, false);
+        students[1] = stu;
+        stu = new Student("Daria", "Iglasta", 998701, true);
+        students[2] = stu;
 
         for (Student student : students) {
             student.niceDisplay();
