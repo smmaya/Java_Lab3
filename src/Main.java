06a0bbcb5dda1ada526c570b310167c348c7b980
Sławomir Majchrzak <smmaya@yahoo.fr>
2021-12-14 08:22:38 +0100

    zad 3

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,39 @@ public class Main {
     public static void main(String[] args) {
 
         /* ćw 3 klasy */
+        System.out.println("Wypisujemy wartości obu konstruktorów, pX i pX, pY, pZ");
+        Punkt punktA = new Punkt(7);
+        punktA.toString();
+
+        Punkt punktB = new Punkt(1, 2, 3);
+        punktB.toString();
+        System.out.println("");
+
+        System.out.println("Nadpisujemy wartości setterami:");
+        punktA.setpX(2);
+        punktA.setpY(4);
+        punktA.setpZ(6);
+        punktA.toString();
+
+        System.out.println("");
+        System.out.println("Wartości punktu A:");
+        System.out.println("X punktu A: " + punktA.getpX());
+        System.out.println("Y punktu A: " + punktA.getpY());
+        System.out.println("Z punktu A: " + punktA.getpZ());
+        System.out.println("");
+        System.out.println("Wartości punktu B:");
+        System.out.println("X punktu B: " + punktB.getpX());
+        System.out.println("X punktu B: " + punktB.getpY());
+        System.out.println("X punktu B: " + punktB.getpZ());
+        System.out.println("");
+        System.out.println("Suma wartości punktu B:");
+        System.out.println(punktB.suma());
+        System.out.println("Różnica wartości punktu B:");
+        System.out.println(punktB.roznica());
+        System.out.println("Różnica A:");
+        System.out.println(punktA.roznica(punktB.getpX(), punktB.getpY(), punktB.getpZ()));
+        System.out.println("Różnica B:");
+        System.out.println(punktB.roznica(punktB.getpX(), punktB.getpY(), punktB.getpZ()));
 
         /*  zad.1 - Najpierw należy uzupełnić kod w klasie Punkt.java,
             a) stworzyć objekt punktA typu Punkt,
--- src/Punkt.java
+++ src/Punkt.java
@@ -1,6 +1,46 @@
 /* klasa Punkt */
 public class Punkt {
     /* stworzyć zmienne prywatne pX, pY, pZ */
+    int pX;
+    int pY;
+    int pZ;
+
+    public Punkt() {
+    }
+
+    public Punkt(int pX) {
+        this.pX = pX;
+    }
+
+    public Punkt(int pX, int pY, int pZ) {
+        this.pX = pX;
+        this.pY = pY;
+        this.pZ = pZ;
+    }
+
+    public int getpX() {
+        return pX;
+    }
+
+    public void setpX(int pX) {
+        this.pX = pX;
+    }
+
+    public int getpY() {
+        return pY;
+    }
+
+    public void setpY(int pY) {
+        this.pY = pY;
+    }
+
+    public int getpZ() {
+        return pZ * 10;
+    }
+
+    public void setpZ(int pZ) {
+        this.pZ = pZ;
+    }
 
     /* stworzyć
         a) pusty konstruktor,
@@ -12,7 +52,20 @@ public class Punkt {
         podpowiedz ALT + INSERT można wybrać generowanie automatyczne
         konstruktora, getterów, setterów, nadpisywanie metody toString() i inne ...
     */
+    public int suma(){
+        return pX + pY + pZ;
+    }
+    public int roznica(){
+        return pX - pY - pZ;
+    }
+    public int roznica(int x, int y, int z){
+        return (pX * x) - (pY * y) - (pZ * z);
+    }
 
+    public String toString(){
+        System.out.println("X:" + pX + " Y:" + pY + " Z:" + pZ);
+        return null;
+    }
     /* Stworzyć metody
         a) suma - sumująca wszystkie wartości w klasie
         b) różnica - metody przeciążone roznica(){} i roznica(int x, int y, int z){}
